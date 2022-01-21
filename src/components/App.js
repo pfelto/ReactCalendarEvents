8b721b80ebb33e61b28d19898a02003f2fe21c93
pfelto <paul.r.felton@gmail.com>
2022-01-21 11:08:07 -0500

    Working on setting up a React Controlled Form to be able to supply library with correct fields

--- src/components/App.js
+++ src/components/App.js
@@ -2,22 +2,35 @@
 import React, { useState } from "react";
 import { ICalendar } from "datebook";
 
-export const App = () => {
-  const [calendarEvent, setCalendarEvent] = useState({});
+const emptyCalendar = {
+  title: "",
+  description: null,
+  location: "",
+  start: "",
+  end: null,
+  recurrence: {
+    frequency: null,
+    interval: null,
+    count: null,
+    monthdays: null,
+    weekstart: null,
+  },
+};
 
-  function createICS() {
+export const App = () => {
+  const [calendarEvent, setCalendarEvent] = useState(emptyCalendar);
+
+  function createICS(event) {
     const config = {
-      title: "Happy Hour",
-      location: "The Bar, New York, NY",
+      title: event.title,
+      location: event.location,
       description: "Let's blow off some steam with a tall cold one!",
-      start: new Date("2022-07-08T19:00:00"),
-      end: new Date("2022-07-08T23:30:00"),
+      start: new Date(event.start),
+      end: new Date(event.start),
       // an event that recurs every two weeks:
-      recurrence: {
-        frequency: "WEEKLY",
-        interval: 2,
-      },
     };
+    console.log(event);
+
     const icalendar = new ICalendar(config);
 
     icalendar.download();
@@ -31,19 +44,60 @@ export const App = () => {
         <form
           onSubmit={(e) => {
             e.preventDefault();
-            createICS();
+            createICS(calendarEvent);
           }}
         >
           <label htmlFor="title">Event Title</label>
-          <input id="title" type="text"></input>
+          <input
+            id="title"
+            type="text"
+            value={calendarEvent.title}
+            onChange={(e) =>
+              setCalendarEvent((calendarEvent) => ({
+                ...calendarEvent,
+                title: e.target.value,
+              }))
+            }
+          ></input>
           <label htmlFor="location">Event Location</label>
-          <input id="location" type="text"></input>
+          <input
+            id="location"
+            type="text"
+            value={calendarEvent.location}
+            onChange={(e) =>
+              setCalendarEvent((calendarEvent) => ({
+                ...calendarEvent,
+                location: e.target.value,
+              }))
+            }
+          ></input>
           <label htmlFor="desc">Event Description</label>
-          <textarea id="desc" style={{ resize: "none" }}></textarea>
+          <textarea
+            id="desc"
+            style={{ resize: "none" }}
+            value={calendarEvent.description}
+            onChange={(e) =>
+              setCalendarEvent((calendarEvent) => ({
+                ...calendarEvent,
+                description: e.target.value,
+              }))
+            }
+          ></textarea>
           <div className="dateSection">
             <div className="dates">
               <label htmlFor="sDate">Start Date</label>
-              <input id="sDate" type="date"></input>
+              <input
+                id="sDate"
+                type="datetime-local"
+                value={calendarEvent.start}
+                onChange={(e) =>
+                  setCalendarEvent((calendarEvent) => ({
+                    ...calendarEvent,
+                    start: e.target.value,
+                  }))
+                }
+                required
+              ></input>
               <div>
                 <input id="allday" type="checkbox" defaultChecked></input>
                 <label htmlFor="allday">All Day Event</label>
@@ -51,7 +105,7 @@ export const App = () => {
             </div>
             <div className="dates">
               <label htmlFor="eDate">End Date</label>
-              <input id="eDate" type="date"></input>
+              <input id="eDate" type="datetime-local"></input>
             </div>
           </div>
           <div>
